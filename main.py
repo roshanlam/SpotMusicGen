ecf9df3c99e75a9d697821ac8f31d35824dca9ec
Roshan <lamichhaner40@gmail.com>
2021-06-08 21:15:08 -0400

    It works

---
+++ main.py
@@ -0,0 +1,20 @@
+from spot import Spotify
+from yt import Youtube
+
+def main():
+    sp = Spotify()
+    yt = Youtube()
+
+    yt_playlist_id = input("Enter youtube playlist id: ")
+    spotify_playlist_name = input("Enter a name for your spotify playlist: ")
+    songs = yt.get_songs_from_playlist(yt_playlist_id)
+    spotify_playlist_id = sp.create_playlist(spotify_playlist_name)
+
+    for song in songs:
+        song_uri = sp.get_song_uri(song.artist, song.title)
+        was_added = sp.add_song_to_playlist(song_uri, spotify_playlist_id)
+        if was_added:
+            print(f'{song.artist} - {song.title} was added to playlist.')
+
+if __name__ == "__main__":
+    main()
